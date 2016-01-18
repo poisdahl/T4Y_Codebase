783fbffe049ba279f80d23da2ce27df6cc973e3b
Sebastian Fiss <sebastianfiss@gmail.com>
2016-01-18 11:08:49 +0100

    deleted old stuff

--- NewBus/src/qrreader/IQrDecoder.java
+++
@@ -1,7 +0,0 @@
-package qrreader;
-
-import java.awt.image.BufferedImage;
-
-public interface IQrDecoder {
-	String decode(BufferedImage image);
-}
--- NewBus/src/qrreader/IWebcamReader.java
+++
@@ -1,8 +0,0 @@
-package qrreader;
-
-import java.awt.image.BufferedImage;
-
-public interface IWebcamReader {
-	// possible transform image using ImageIO
-	BufferedImage readImageFromCam();
-}
