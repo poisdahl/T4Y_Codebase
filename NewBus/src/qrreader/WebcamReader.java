173a59af2256c3cf13257ea6d1b613864877f703
Sebastian Fiss <sebastianfiss@gmail.com>
2016-01-18 10:18:19 +0100

    added webcam reader dependency and impl

--- NewBus/src/qrreader/WebcamReader.java
+++ NewBus/src/qrreader/WebcamReader.java
@@ -2,12 +2,15 @@ package qrreader;
 
 import java.awt.image.BufferedImage;
 
+import com.github.sarxos.webcam.Webcam;
+
 public class WebcamReader implements IWebcamReader {
 
 	@Override
 	public BufferedImage readImageFromCam() {
-		// TODO Auto-generated method stub
-		return null;
+		Webcam webcam = Webcam.getDefault();
+		webcam.open();
+		return webcam.getImage();
 	}
 
 
